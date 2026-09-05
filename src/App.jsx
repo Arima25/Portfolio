019dcf15f89ba5c1bc95c8f7eaa5a9b3db18ab4b
AWT_ Poke <jwang769@ucsc.edu>
2026-09-05 18:13:05 +0800

    Revert "Add animated Hello Kitty sprite to 3D background"
    This reverts commit 458ef3f537db21942e0b1f22696c88bdf499d7aa.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import { ScrollControls, Cloud, Float, Scroll, Sparkles, useScroll } from '@react-three/drei'
 import { useEffect, useRef, useState } from 'react'
-import { CanvasTexture } from 'three'
 import { Overlay } from './Overlay'
 import { TopNav } from './TopNav'
 import './App.css'
@@ -14,134 +13,6 @@ function ScrollBridge({ onReady }) {
     onReady(scroll.el)
   }, [scroll.el, onReady])
   return null
-}
-
-// Hello Kitty sprite with canvas-based drawing
-function HelloKittySprite() {
-  const meshRef = useRef()
-  const [texture, setTexture] = useState(null)
-
-  useEffect(() => {
-    const canvas = document.createElement('canvas')
-    canvas.width = 256
-    canvas.height = 300
-    const ctx = canvas.getContext('2d')
-
-    ctx.fillStyle = '#FFF8DC'
-    ctx.fillRect(0, 0, 256, 300)
-
-    // Head
-    ctx.fillStyle = '#FFF8DC'
-    ctx.strokeStyle = '#FFB6C1'
-    ctx.lineWidth = 2
-    ctx.beginPath()
-    ctx.arc(128, 110, 70, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-
-    // Ears
-    ctx.beginPath()
-    ctx.arc(70, 50, 28, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.arc(186, 50, 28, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-
-    // Ear inner
-    ctx.fillStyle = '#FFB6C1'
-    ctx.beginPath()
-    ctx.arc(70, 50, 18, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.beginPath()
-    ctx.arc(186, 50, 18, 0, Math.PI * 2)
-    ctx.fill()
-
-    // Eyes
-    ctx.fillStyle = '#000'
-    ctx.beginPath()
-    ctx.arc(100, 95, 7, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.beginPath()
-    ctx.arc(156, 95, 7, 0, Math.PI * 2)
-    ctx.fill()
-
-    // Nose
-    ctx.fillStyle = '#FFB6C1'
-    ctx.beginPath()
-    ctx.arc(128, 115, 6, 0, Math.PI * 2)
-    ctx.fill()
-
-    // Mouth
-    ctx.strokeStyle = '#FFB6C1'
-    ctx.lineWidth = 3
-    ctx.lineCap = 'round'
-    ctx.beginPath()
-    ctx.quadraticCurveTo(110, 135, 90, 130)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.quadraticCurveTo(146, 135, 166, 130)
-    ctx.stroke()
-
-    // Body
-    ctx.fillStyle = '#FFF8DC'
-    ctx.strokeStyle = '#FFB6C1'
-    ctx.lineWidth = 2
-    ctx.beginPath()
-    ctx.ellipse(128, 185, 48, 60, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-
-    // Bow
-    ctx.fillStyle = '#FF69B4'
-    ctx.beginPath()
-    ctx.ellipse(85, 35, 14, 18, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.beginPath()
-    ctx.ellipse(171, 35, 14, 18, 0, 0, Math.PI * 2)
-    ctx.fill()
-
-    ctx.fillStyle = '#FF1493'
-    ctx.beginPath()
-    ctx.arc(128, 35, 8, 0, Math.PI * 2)
-    ctx.fill()
-
-    // Arms
-    ctx.fillStyle = '#FFF8DC'
-    ctx.strokeStyle = '#FFB6C1'
-    ctx.lineWidth = 2
-    ctx.beginPath()
-    ctx.ellipse(75, 175, 14, 28, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.ellipse(181, 175, 14, 28, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-
-    // Feet
-    ctx.beginPath()
-    ctx.ellipse(100, 250, 16, 18, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.ellipse(156, 250, 16, 18, 0, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.stroke()
-
-    const canvasTexture = new CanvasTexture(canvas)
-    setTexture(canvasTexture)
-  }, [])
-
-  if (!texture) return null
-
-  return (
-    <mesh ref={meshRef} position={[-1, -2, 0]}>
-      <planeGeometry args={[1, 1.2]} />
-      <meshStandardMaterial map={texture} transparent />
-    </mesh>
-  )
 }
 
 // A custom component for background elements that move when you scroll
@@ -175,10 +46,6 @@ function BackgroundElements() {
             <sphereGeometry args={[0.5, 32, 32]} />
             <meshStandardMaterial color="#FFC8DD" />
           </mesh>
-        </Float>
-        {/* HELLO KITTY SPRITE (next to the blue cinnamon roll) */}
-        <Float speed={2} rotationIntensity={0.8} floatIntensity={1.2}>
-          <HelloKittySprite />
         </Float>
       </Scroll>
     </group>
